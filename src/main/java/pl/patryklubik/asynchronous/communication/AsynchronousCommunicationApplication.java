9184d6544211e82dba4f0337812d5b88d6083843
Patryk Łubik <patryk.lubik@wp.pl>
2021-07-17 19:34:41 +0200

    skeleton

---
+++ src/main/java/pl/patryklubik/asynchronous/communication/AsynchronousCommunicationApplication.java
@@ -0,0 +1,13 @@
+package pl.patryklubik.asynchronous.communication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsynchronousCommunicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsynchronousCommunicationApplication.class, args);
+	}
+
+}
